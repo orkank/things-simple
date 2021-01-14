1d9b8d577e9f5b9bbc05ca5d381bb928e45c06ef
Taner Akhan <taner.akhan@sozcu.com.tr>
2021-01-14 11:08:06 +0300

    added splice readme

---
+++ vanillaJS/splice/readme.md
@@ -0,0 +1,3 @@
+## array içerisinde sondan başlayarak verilen index değeri kadar değer döndürür.
+
+## array değer çıkartılan değerlerden sonra kalan kadar döner
---
+++ vanillaJS/splice/splice.js
@@ -0,0 +1,7 @@
+const arr = [1,2,3,4,5,6,7,8,9];
+
+arr.splice(-1)  // [9]
+                // console.log(arr) => [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+
+arr.splice(-2)  // [8,9]
+                // console.log(arr) => [ 1, 2, 3, 4, 5, 6, 7]
